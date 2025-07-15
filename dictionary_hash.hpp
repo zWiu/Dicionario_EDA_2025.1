6dabf357b8cbc0e68952207f57d039ee4ff61999
willgner <willgnersf@alu.ufc.br>
2025-07-14 21:52:58 -0300

    fix: fixing show of hash dictionary

--- dictionary_hash.hpp
+++ dictionary_hash.hpp
@@ -4,12 +4,15 @@
 #include <utility>
 #include <functional>
 #include <list>
+#include <chrono>
 
 template <template <typename, typename, typename> class Structure, typename Key, typename Value, typename Hash = std::hash<Key>>
 class Dictionary_Hash
 {
 private:
     Structure<Key, Value, Hash> dictionary;
+    // constante de tempo para o ínicio 
+    const std::chrono::milliseconds start_time;
 
 public:
     // Construtor vazio
@@ -53,11 +56,11 @@ public:
 
 template <template <typename, typename, typename> class Structure, typename Key, typename Value, typename Hash>
 Dictionary_Hash<Structure, Key, Value, Hash>::Dictionary_Hash()
-    : dictionary{} {}
-
+    : dictionary{}, start_time{std::chrono::high_resolution_clock::now()} {}
 template <template <typename, typename, typename> class Structure, typename Key, typename Value, typename Hash>
 Dictionary_Hash<Structure, Key, Value, Hash>::Dictionary_Hash(std::list<std::pair<Key, Value>> initial_list)
 {
+    start_time = std::chrono::high_resolution_clock::now();
     for (const auto &par : initial_list)
     {
         dictionary.add(par.first, par.second);
@@ -111,6 +114,10 @@ size_t Dictionary_Hash<Structure, Key, Value, Hash>::size()
 template <template <typename, typename, typename> class Structure, typename Key, typename Value, typename Hash>
 void Dictionary_Hash<Structure, Key, Value, Hash>::show()
 {
+    std::cout << "Quantidade de elementos: " << dictionary.size() << std::endl;
+    std::cout << "Comparações entre chaves realizadas(durante a construção do dicionário): " << dictionary.get_cont_comparator() << std::endl;
+    std::cout << "Colisões ocorridas(durante a construção do dicionário): " << dictionary.get_cont_collisions() << std::endl;
+    std::cout << "Tempo de construção do dicionário(até o momento): " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-start_time) << std::endl << std::endl;
     dictionary.show();
 }
 
--- main.cpp
+++ main.cpp
@@ -4,74 +4,32 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <sstream>
 #include <unicode/unistr.h>
 #include <unicode/uchar.h>
-#include <unicode/ustring.h>    
+#include <unicode/ustring.h>
 
 using namespace std;
-
-struct Pares
-{
-    int k;
-    string v;
-
-    Pares(int k = 0, string v = "")
-    {
-        this->k = k;
-        this->v = v;
-    }
-
-    Pares(const Pares &p)
-    {
-        this->k = p.k;
-        this->v = p.v;
-    }
-
-    Pares &operator=(const Pares &p)
-    {
-        this->k = p.k;
-        this->v = p.v;
-
-        return *this;
-    }
-
-    bool operator<(const Pares &p)
-    {
-        return this->v < p.v;
-    }
-
-    bool operator>(const Pares &p)
-    {
-        return this->v > p.v;
-    }
-
-    bool operator==(const Pares &p)
-    {
-        return this->v == p.v;
-    }
-
-    bool operator!=(const Pares &p)
-    {
-        return this->v != p.v;
-    }
-
-    friend std::ostream &operator<<(std::ostream &os, const Pares &p)
-    {
-        os << "(" << p.k << ", " << p.v << ")";
-        return os;
-    }
-};
 
 namespace std
 {
     template <>
-    struct hash<Pares>
+    struct hash<icu::UnicodeString>
     {
-        size_t operator()(const Pares &p) const
+        size_t operator()(const icu::UnicodeString &uni_str) const
         {
-            return hash<int>()(p.k) ^ (hash<std::string>()(p.v) << 1);
+            std::string utf8;
+            uni_str.toUTF8String(utf8);
+            return std::hash<std::string>()(utf8);
         }
     };
+}
+
+std::ostream &operator<<(std::ostream &os, const icu::UnicodeString &ustr)
+{
+    std::string utf8;
+    ustr.toUTF8String(utf8);
+    return os << utf8;
 }
 #include "./structures/ChainedHashTable.hpp"
 #include "./structures/OpenAdressingHashTable.hpp"
@@ -82,7 +40,7 @@ namespace std
 
 void listArchive(string nameFile)
 {
-    ifstream file(nameFile);
+    ifstream file("livros/" + nameFile + ".txt");
     if (!file.is_open())
     {
         cerr << "Falha ao abrir arquivo: " << nameFile << endl;
@@ -92,30 +50,33 @@ void listArchive(string nameFile)
     string entrada;
     vector<icu::UnicodeString> vec_unicode_names;
     icu::UnicodeString tratada, normalizada;
-    
+
     while (getline(file, entrada))
     {
         tratada.remove();
         normalizada = icu::UnicodeString::fromUTF8(entrada);
-        for(int32_t i = 0; i < normalizada.length(); i++)
+        for (int32_t i = 0; i < normalizada.length(); i++)
         {
             UChar c = normalizada.charAt(i);
 
-            if(u_isalpha(c)) {
+            if (u_isalpha(c))
+            {
                 tratada.append(c);
             }
 
-            if(c == '-' && i > 0 && i < normalizada.length() - 1) {
-                if(u_isalpha(normalizada.charAt(i-1)) && u_isalpha(normalizada.charAt(i+1)))
+            if (c == '-' && i > 0 && i < normalizada.length() - 1)
+            {
+                if (u_isalpha(normalizada.charAt(i - 1)) && u_isalpha(normalizada.charAt(i + 1)))
                     tratada.append(c);
             }
-        
-            if(c == ' ' && !tratada.isEmpty()){
+
+            if (c == ' ' && !tratada.isEmpty())
+            {
                 vec_unicode_names.push_back(tratada.toLower());
                 tratada.remove();
             }
         }
-        if(!tratada.isEmpty())
+        if (!tratada.isEmpty())
             vec_unicode_names.push_back(tratada.toLower());
     }
 
@@ -130,366 +91,60 @@ void listArchive(string nameFile)
         cout << "Número de caracteres em \"" << utf8str << "\": " << codepointCount << "\n";
     }
 }
-void testChainedHashTable()
+
+void dictionary_AVL(string nameFile)
 {
-    ChainedHashTable<Pares, int> tabela(7, 1.5);
-
-    Pares p1(10, "A");
-    Pares p2(7, "B");
-    Pares p3(15, "C");
-    Pares p4(43, "D");
-    Pares p5(5, "E");
-    cout << "\n***TESTE DA CHAINED HASH TABLE***" << endl;
-
-    // Teste de adição
-    cout << "Adicionando elementos:" << endl;
-    if (tabela.add(p1, 2))
-        cout << "Adicionado " << p1 << ", " << 10 << endl;
-    if (tabela.add(p2, 20))
-        cout << "Adicionado " << p2 << ", " << 20 << endl;
-    if (tabela.add(p3, 30))
-        cout << "Adicionado " << p3 << ", " << 30 << endl;
-    if (tabela.add(p4, 40))
-        cout << "Adicionado " << p4 << ", " << 40 << endl;
-    if (tabela.add(p5, 50))
-        cout << "Adicionado " << p5 << ", " << 50 << endl;
-
-    // Mostrando a tabela
-    tabela.show();
-
-    // Tamanho e fator de carga
-    cout << "\nTamanho atual: " << tabela.size() << endl;
-    cout << "Fator de carga: " << tabela.load_factor() << endl;
-
-    // Teste de contains
-    cout << "\nContém p3? " << (tabela.contains(p3) ? "Sim" : "Não") << endl;
-
-    // Teste do método at
-    try
+    ifstream file("livros/" + nameFile + ".txt");
+    if (!file.is_open())
     {
-        cout << "Valor associado a p2: " << tabela.at(p2) << endl;
-    }
-    catch (const exception &e)
-    {
-        cout << e.what() << endl;
+        cerr << "Falha ao abrir arquivo: " << nameFile << endl;
+        return;
     }
 
-    // Teste do operador []
-    cout << "\nAcessando p4 com operador []: " << tabela[p4] << endl;
-    tabela[p4] = 100;
-    cout << "Novo valor de p4: " << tabela[p4] << endl;
+    string entrada;
+    Dictionary_Tree<AVL, icu::UnicodeString, int> avl_unicode;
+    icu::UnicodeString tratada, normalizada;
 
-    // Teste de remoção
-    cout << "\nRemovendo p1..." << endl;
-    bool removido = tabela.remove(p1);
-    cout << (removido ? "Removido com sucesso." : "Não encontrado.") << endl;
-
-    cout << "Contém p1? " << (tabela.contains(p1) ? "Sim" : "Não") << endl;
-
-    // Teste de bucket e bucket_size
-    cout << "\nBucket de p3: " << tabela.bucket(p3) << endl;
-    cout << "Tamanho do bucket de p3: " << tabela.bucket_size(tabela.bucket(p3)) << endl;
-
-    // Teste de rehash forçado via reserve
-    cout << "\nExecutando reserve(50)..." << endl;
-    tabela.reserve(50);
-    cout << "Novo bucket_count: " << tabela.bucket_count() << endl;
-
-    // Contadores:
-    cout << "\nContador de comparações feitas nas chaves: " << tabela.get_cont_comparator() << endl;
-    cout << "Contador de colisões: " << tabela.get_cont_collisions() << endl;
-
-    // Teste de clear
-    cout << "\nLimpando a tabela..." << endl;
-    tabela.clear();
-    cout << "Tabela está vazia? " << (tabela.empty() ? "Sim" : "Não") << endl;
-}
-
-void testOpenAdressingHashTable()
-{
-    OpenAdressingHashTable<Pares, int> tabela(7, 1.5);
-
-    Pares p1(10, "A");
-    Pares p2(7, "B");
-    Pares p3(15, "C");
-    Pares p4(43, "D");
-    Pares p5(5, "E");
-    cout << "\n***TESTE DA HASH TABLE***" << endl;
-
-    // Teste de adição
-    cout << "Adicionando elementos:" << endl;
-    if (tabela.add(p1, 2))
-        cout << "Adicionado " << p1 << ", " << 10 << endl;
-    if (tabela.add(p2, 20))
-        cout << "Adicionado " << p2 << ", " << 20 << endl;
-    if (tabela.add(p3, 30))
-        cout << "Adicionado " << p3 << ", " << 30 << endl;
-    if (tabela.add(p4, 40))
-        cout << "Adicionado " << p4 << ", " << 40 << endl;
-    if (tabela.add(p5, 50))
-        cout << "Adicionado " << p5 << ", " << 50 << endl;
-
-    // Mostrando a tabela
-    tabela.show();
-
-    // Tamanho e fator de carga
-    cout << "\nTamanho atual: " << tabela.size() << endl;
-    cout << "Fator de carga: " << tabela.load_factor() << endl;
-
-    // Teste de contains
-    cout << "\nContém p3? " << (tabela.contains(p3) ? "Sim" : "Não") << endl;
-
-    // Teste do método at
-    try
+    while (getline(file, entrada))
     {
-        cout << "Valor associado a p2: " << tabela.at(p2) << endl;
-    }
-    catch (const exception &e)
-    {
-        cout << e.what() << endl;
+        tratada.remove();
+        normalizada = icu::UnicodeString::fromUTF8(entrada);
+        for (int32_t i = 0; i < normalizada.length(); i++)
+        {
+            UChar c = normalizada.charAt(i);
+
+            if (u_isalpha(c))
+            {
+                tratada.append(c);
+            }
+
+            if (c == '-' && i > 0 && i < normalizada.length() - 1)
+            {
+                if (u_isalpha(normalizada.charAt(i - 1)) && u_isalpha(normalizada.charAt(i + 1)))
+                    tratada.append(c);
+            }
+
+            if (c == ' ' && !tratada.isEmpty())
+            {
+                tratada = tratada.toLower();
+                avl_unicode[tratada] = avl_unicode[tratada] + 1;
+                tratada.remove();
+            }
+        }
+        if (!tratada.isEmpty())
+            avl_unicode[tratada] = avl_unicode[tratada] + 1;
     }
 
-    // Teste do operador []
-    cout << "\nAcessando p4 com operador []: " << tabela[p4] << endl;
-    tabela[p4] = 100;
-    cout << "Novo valor de p4: " << tabela[p4] << endl;
+    std::stringstream buffer;
+    std::streambuf* antigo = std::cout.rdbuf();
 
-    // Teste de remoção
-    cout << "\nRemovendo p1..." << endl;
-    bool removido = tabela.remove(p1);
-    cout << (removido ? "Removido com sucesso." : "Não encontrado.") << endl;
+    std::cout.rdbuf(buffer.rdbuf());
 
-    cout << "Contém p1? " << (tabela.contains(p1) ? "Sim" : "Não") << endl;
+    avl_unicode.show();
 
-    // Teste de rehash forçado via reserve
-    cout << "\nExecutando reserve(50)..." << endl;
-    tabela.reserve(50);
-    cout << "Novo bucket_count: " << tabela.bucket_count() << endl;
+    std::cout.rdbuf(antigo);
 
-    // Contadores:
-    cout << "\nContador de comparações feitas nas chaves: " << tabela.get_cont_comparator() << endl;
-    cout << "Contador de colisões: " << tabela.get_cont_collisions() << endl;
-
-    // Teste de clear
-    cout << "\nLimpando a tabela..." << endl;
-    tabela.clear();
-    cout << "Tabela está vazia? " << (tabela.empty() ? "Sim" : "Não") << endl;
-}
-
-void testAVL()
-{
-    AVL<Pares, int> arvore;
-
-    Pares p1(10, "A");
-    Pares p2(7, "B");
-    Pares p3(15, "C");
-    Pares p4(43, "D");
-    Pares p5(5, "E");
-    cout << "\n***TESTE DA AVL***" << endl;
-
-    // Teste de adição
-    cout << "Adicionando elementos:" << endl;
-    arvore.add(p1, 2);
-    cout << "Adicionado " << p1 << ", " << 10 << endl;
-    arvore.add(p2, 20);
-    cout << "Adicionado " << p2 << ", " << 20 << endl;
-    arvore.add(p3, 30);
-    cout << "Adicionado " << p3 << ", " << 30 << endl;
-    arvore.add(p4, 40);
-    cout << "Adicionado " << p4 << ", " << 40 << endl;
-    arvore.add(p5, 50);
-    cout << "Adicionado " << p5 << ", " << 50 << endl;
-
-    cout << "Criando cópia da primeira árvore..." << endl;
-    AVL<Pares, int> arvore_copia(arvore);
-
-    // Estrutura da árvore
-    arvore.show_tree();
-
-    // Teste de remoção
-    cout << "\nRemovendo elementos:" << endl;
-    arvore.remove(p3);
-    cout << "Elemento " << p3 << " removido da árvore 1" << endl;
-    arvore_copia.remove(p2);
-    cout << "Elemento " << p2 << " removido da árvore 2" << endl;
-
-    // Teste do construtor de cópia
-    cout << "\nResultado das remoções: " << endl;
-    cout << "Árvore 1:" << endl;
-    arvore.show();
-    cout << "Árvore 2:" << endl;
-    arvore_copia.show();
-
-    // Teste de contains
-    cout << "Contém " << p1 << " na árvore 1? " << (arvore.contains(p1) ? "Sim" : "Não") << endl;
-    cout << "Contém " << p3 << " na árvore 1? " << (arvore.contains(p3) ? "Sim" : "Não") << endl;
-
-    // Teste de update
-    cout << "\nTestando a função de update" << endl;
-    arvore.update(p2, 50);
-    cout << "Elemento " << p2 << " atualizado na árvore 1" << endl;
-    arvore.update(p1, 130);
-    cout << "Elemento " << p1 << " atualizado na árvore 1" << endl;
-    try
-    {
-        arvore.update(p3, 70);
-    }
-    catch (const exception &e)
-    {
-        cout << "Tentativa de atualização do elemento " << p3 << endl;
-    }
-
-    cout << "\nTeste do operator[]" << endl;
-    cout << "Valor de " << p2 << " :" << arvore[p2] << endl;
-    cout << "Valor de " << p3 << " :" << arvore[p3] << endl;
-
-    cout << "\nResultado das atualizações: " << endl;
-    cout << "Árvore 1:" << endl;
-    arvore.show();
-
-    // Teste de contadores
-    cout << "\nContadores da árvore 1" << endl;
-    cout << "Contador de comparações feitas nas chaves: " << arvore.get_cont_comparator() << endl;
-    cout << "Contador de rotações realizadas na árvore: " << arvore.get_cont_rotation() << endl;
-
-    cout << "\nContadores da árvore 2" << endl;
-    cout << "Contador de comparações feitas nas chaves: " << arvore_copia.get_cont_comparator() << endl;
-    cout << "Contador de rotações realizadas na árvore: " << arvore_copia.get_cont_rotation() << endl;
-
-    // Teste de clear
-    cout << "\nLimpando a arvore..." << endl;
-    arvore.clear();
-    cout << "Árvore está vazia? " << (arvore.empty() ? "Sim" : "Não") << endl;
-}
-
-void testRBT()
-{
-    RBT<Pares, int> arvore;
-
-    Pares p1(10, "A");
-    Pares p2(7, "B");
-    Pares p3(15, "C");
-    Pares p4(43, "D");
-    Pares p5(5, "E");
-    cout << "\n***TESTE DA RBT***" << endl;
-
-    // Teste de adição
-    cout << "Adicionando elementos:" << endl;
-    arvore.add(p1, 2);
-    cout << "Adicionado " << p1 << ", " << 2 << endl;
-    arvore.add(p2, 20);
-    cout << "Adicionado " << p2 << ", " << 20 << endl;
-    arvore.add(p3, 30);
-    cout << "Adicionado " << p3 << ", " << 30 << endl;
-    arvore.add(p4, 40);
-    cout << "Adicionado " << p4 << ", " << 40 << endl;
-    arvore.add(p5, 50);
-    cout << "Adicionado " << p5 << ", " << 50 << endl;
-
-    // Teste de cópia
-    RBT<Pares, int> arvore_copia(arvore);
-
-    // Estrutura da árvore
-    cout << "\nÁrvore 1:" << endl;
-    arvore.show_tree();
-    cout << "Árvore 2:" << endl;
-    arvore_copia.show_tree();
-
-    // Teste de remoção
-    cout << "\nRemovendo elementos:" << endl;
-    arvore.remove(p3);
-    cout << "Elemento " << p3 << " removido da árvore 1" << endl;
-    arvore_copia.remove(p2);
-    cout << "Elemento " << p2 << " removido da árvore 1" << endl;
-
-    // Teste do construtor de cópia
-    cout << "\nResultado das remoções: " << endl;
-    cout << "Árvore 1:" << endl;
-    arvore.show();
-    cout << "Árvore 2:" << endl;
-    arvore_copia.show();
-
-    // Teste de contains
-    cout << "Contém " << p1 << " na árvore 1? " << (arvore.contains(p1) ? "Sim" : "Não") << endl;
-    cout << "Contém " << p3 << " na árvore 1? " << (arvore.contains(p3) ? "Sim" : "Não") << endl;
-
-    // Teste de update
-    cout << "\nTestando a função de update" << endl;
-    arvore.update(p2, 50);
-    cout << "Elemento " << p2 << " atualizado na árvore 1" << endl;
-    arvore.update(p1, 130);
-    cout << "Elemento " << p1 << " atualizado na árvore 1" << endl;
-    try
-    {
-        arvore.update(p3, 70);
-    }
-    catch (const invalid_argument &e)
-    {
-        cout << "Tentativa de atualização do elemento " << p3 << endl;
-    }
-
-    cout << "\nTeste do operator[]" << endl;
-    cout << "Valor de " << p2 << " :" << arvore[p2] << endl;
-    cout << "Valor de " << p3 << " :" << arvore[p3] << endl;
-
-    cout << "\nResultado das atualizações: " << endl;
-    cout << "Árvore 1:" << endl;
-    arvore.show();
-    cout << "Árvore 2:" << endl;
-    arvore_copia.show();
-
-    // Teste de contadores
-    cout << "\nContadores da árvore 1" << endl;
-    cout << "Contador de comparações feitas nas chaves: " << arvore.get_cont_comparator() << endl;
-    cout << "Contador de rotações realizadas na árvore: " << arvore.get_cont_rotation() << endl;
-
-    cout << "\nContadores da árvore 2" << endl;
-    cout << "Contador de comparações feitas nas chaves: " << arvore_copia.get_cont_comparator() << endl;
-    cout << "Contador de rotações realizadas na árvore: " << arvore_copia.get_cont_rotation() << endl;
-
-    // Teste de clear
-    cout << "\nLimpando a arvore..." << endl;
-    arvore.clear();
-    cout << "Árvore está vazia? " << (arvore.empty() ? "Sim" : "Não") << endl;
-}
-
-void testeDictionaryOpenHash()
-{
-    Dictionary_Hash<OpenAdressingHashTable, string, int> dicionario;
-
-    // Testanto a inserção
-    dicionario.insert("Pei", 2);
-
-    // Testando o retorno
-    cout << dicionario.at("Pei") << endl;
-
-    dicionario.show();
-
-    dicionario.update("Pei", 5);
-
-    cout << dicionario["Pei"] << endl;
-
-    // Testando a remoção
-    dicionario.remove("Pei");
-    cout << "Concluido" << endl;
-}
-
-void testeDictionaryAVL()
-{
-    Dictionary_Tree<AVL, string, int> dicionario;
-    cout << "Testando inserção" << endl;
-    // Testanto a inserção
-    dicionario.insert("Pei", 2);
-    cout << "Testante update" << endl;
-    // Testando a atualização
-    dicionario.update("Pei", 10);
-    cout << "testando operator[]" << endl;
-    // Testando o retorno
-    cout << dicionario.at("Pei") << endl;
-
-    dicionario.show();
-    // Testando a remoção
-    dicionario.remove("Pei");
+    std::string textoCapturado = buffer.str();
 }
 
 int main(int argc, char *argv[])
@@ -504,24 +159,24 @@ int main(int argc, char *argv[])
     {
         cout << argv[i] << endl;
         str = argv[i];
-        if (str == "teste_chained_hash")
-            testChainedHashTable();
-        else if (str == "teste_open_hash")
-            testOpenAdressingHashTable();
-        else if (str == "teste_avl")
-            testAVL();
-        else if (str == "teste_rbt")
-            testRBT();
-        else if (str == "teste_dicionario_open_hash")
-            testeDictionaryOpenHash();
-        else if (str == "teste_dicionario_avl")
-            testeDictionaryAVL();
-        else if (str == "liste")
+        if (str == "liste")
         {
             if (i + 1 < argc)
             {
                 i++;
                 listArchive(argv[i]);
+            }
+            else
+            {
+                cerr << "Erro: 'liste' exige um argumento de caminho" << endl;
+            }
+        }
+        else if (str == "dictionary_avl")
+        {
+            if (i + 1 < argc)
+            {
+                i++;
+                dictionary_AVL(argv[i]);
             }
             else
             {
